c730e894568d5fbc18d68eed898a3168d0c5451e
Juber8 <90597119+Juber8@users.noreply.github.com>
2021-11-17 17:12:58 +0530

    Update even and odd number.py

--- even and odd number.py
+++ even and odd number.py
@@ -31,3 +31,36 @@ for i in range(0,n+1):
     if i%2!=0:
         l.append(i)
 print(str(l))
+# print even number for given range
+# for even range take i=2 for odd take i=1 and increment by 2
+'''n=int(input('enter the range'))
+i=1
+while(i<=n):
+    print(i)
+    i+=2
+
+#By using for loop
+n=int(input('enter the range'))
+output=[]
+for i in range(0,n+1):
+    if i%2==0:
+        output.append(i)
+
+print(output)
+
+# by using function
+def even(n):
+    l=[]
+    for i in range(0,n+1):
+        if i%2==0:
+            l.append(i)
+    return l
+print(even(50))
+
+# for odd number
+n=int(input('enter the number'))
+l=[]
+for i in range(0,n+1):
+    if i%2!=0:
+        l.append(i)
+print(str(l))
